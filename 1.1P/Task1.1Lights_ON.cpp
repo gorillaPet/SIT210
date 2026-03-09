273331c0a2fd023cb7b5b5a25d69d4736c7d5737
gorillaPet <gorillapet@proton.me>
2026-03-09 23:50:59 +1100

    initial outline of program in single file

---
+++ 1.1P/Task1.1Lights_ON.cpp
@@ -0,0 +1,50 @@
+//declare lights, button, and button state. 
+
+const int hallway = 5;
+const int door = 12;
+const int onOff = 13;
+bool pressed = false;
+
+//modularise later in a separate file
+void lightOn(int light)
+{
+  digitalWrite(light, HIGH);
+}
+
+void lightOff(int light)
+{
+  digitalWrite(light, LOW);
+}
+
+void setup()
+{
+  pinMode(onOff, INPUT_PULLUP);
+  pinMode(door, OUTPUT);
+  pinMode(hallway, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (digitalRead(onOff) == LOW)
+  {
+    Serial.println("Lights on.");
+    pressed = true;
+  }
+
+  if (pressed)
+  {
+    lightOn(door);
+    lightOn(hallway);
+    delay(10000);
+
+    lightOff(door);
+    Serial.println("Door light off.");
+    delay(20000);
+
+    lightOff(hallway);
+    Serial.println("Hall light off.");
+    pressed = false;
+    delay(1000);
+  }
+}
